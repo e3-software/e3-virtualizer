5a7a072fcca213a9d1067d00272a68dc54eebee4
rchandler-eb <rchandler@eventbrite.com>
2024-04-25 08:25:57 -0500

    adds clerk auth

--- app/e3/layout.tsx
+++ app/e3/layout.tsx
@@ -1,4 +1,5 @@
  import Sidebar from '@/app/ui/sidebar/sidebar';
+ import { UserButton } from "@clerk/nextjs"
 
 /**
  * 
@@ -20,11 +21,19 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
             <Sidebar />
           </div>
       </div>
-      <main className="py-10 lg:pl-72">
+
+      <div className="lg:pl-72">
+        <div className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
+        <div className="ml-auto">
+          <UserButton />
+        </div>
+        </div>
+        <main className="py-10">
           <div className="px-4 sm:px-6 lg:px-8">
             {children}
           </div>
-      </main>
+        </main>
+      </div>
     </div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { inter } from "./ui/fonts";
 import "@/app/ui/globals.css";
+import { ClerkProvider } from '@clerk/nextjs'
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,8 +14,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full bg-white">
-      <body className={`${inter.className} h-full`}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" className="h-full bg-white">
+        <body className={`${inter.className} h-full`}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
