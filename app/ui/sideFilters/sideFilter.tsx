c591a23b3f246405738013965d10ea23c8958b5e
Ryann Chandler <ryann.chandler@gmail.com>
2024-05-21 21:21:53 -0500

    maps added

--- app/ui/sideFilters/sideFilter.tsx
+++ app/ui/sideFilters/sideFilter.tsx
@@ -1,10 +1,32 @@
+"use client";
 import { Fragment, useState } from "react";
-import { Button, Dialog, Transition } from "@headlessui/react";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import { AdjustmentsHorizontalIcon } from "@heroicons/react/24/outline";
+import { APIProvider, Map, Marker } from "@vis.gl/react-google-maps";
+
+import {
+  Popover,
+  Button,
+  Dialog,
+  Transition,
+  PopoverButton,
+  PopoverPanel,
+  Description,
+  Field,
+  Fieldset,
+  Input,
+  Label,
+  Legend,
+  Select,
+  Textarea,
+} from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import clsx from "clsx";
 
 const SideFilters = () => {
   const [open, setOpen] = useState(false);
+  const position = { lat: 36.005771, lng: -86.631136 };
+
   return (
     <>
       <Button
@@ -42,7 +64,7 @@ const SideFilters = () => {
                           <div className="ml-3 flex h-7 items-center">
                             <button
                               type="button"
-                              className="relative rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                              className="relative rounded-md  text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                               onClick={() => setOpen(false)}
                             >
                               <span className="absolute -inset-2.5" />
@@ -55,8 +77,46 @@ const SideFilters = () => {
                           </div>
                         </div>
                       </div>
-                      <div className="relative mt-6 flex-1 px-4 sm:px-6">
-                        {/* Your content */}
+                      <div className="px-4 sm:px-6">
+                        <Fieldset className="space-y-4 rounded-xl bg-black/5 p-10">
+                          <Field>
+                            <Label className="text-sm/6 font-medium text-gray-900">
+                              ZipCode
+                            </Label>
+                            <Input
+                              className={clsx(
+                                "mt-3 block w-full rounded-lg border-none bg-black/5 py-1.5 px-3 text-sm/6 text-gray-900",
+                                "focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-black/25",
+                              )}
+                            />
+                          </Field>
+                          <Field>
+                            <Label className="text-sm/6 font-medium text-gray-900">
+                              Radius in Miles
+                            </Label>
+                            <Input
+                              type="number"
+                              className={clsx(
+                                "mt-3 block w-full rounded-lg border-none bg-black/5 py-1.5 px-3 text-sm/6 text-gray-900",
+                                "focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-black/25",
+                              )}
+                            />
+                          </Field>
+                        </Fieldset>
+                        <div className="p-3">
+                          <button className="block rounded-lg py-2 px-3 transition hover:bg-black/5 w-full outline-black/25 outline">
+                            <p className="font-semibold text-gray-900">
+                              Lets DO IT
+                            </p>
+                          </button>
+                        </div>
+                      </div>
+                      <div className="relative mt-6 flex-1 px-4 sm:px-6 flex-col">
+                        <APIProvider apiKey="AIzaSyDZu5_-pOAL3VB-1mB6cE6IgBHszSFicFU">
+                          <Map defaultCenter={position} defaultZoom={10}>
+                            <Marker position={position} />
+                          </Map>
+                        </APIProvider>
                       </div>
                     </div>
                   </Dialog.Panel>
