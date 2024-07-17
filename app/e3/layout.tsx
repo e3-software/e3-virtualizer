6047446ee3cb441001d1ec9c5d9a64e3e51c13ec
Kristen Chandler <krismchandler@gmail.com>
2024-07-17 14:22:08 -0500

    got light/dark mode toggle functioning. it is currently located in the UserBlock. working now to find the right colors for both modes.

--- app/e3/layout.tsx
+++ app/e3/layout.tsx
@@ -1,15 +1,16 @@
 import Navigation from "@/app/ui/navigation";
+import React from 'react'
 
 const BaseLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <div className="min-h-full bg-gray-50">
-        <div className="bg-astral-800 pb-32">
+      <div className="min-h-full bg-gray-50 dark:bg-slate-900 h-screen w-full p-6 transition-colors duration-500 ease-in-out">
+        <div className="bg-astral-800 dark:bg-astral-800 pb-32">
           <Navigation />
         </div>
 
         <main className="-mt-32">
-          <div className="mx-auto max-w-7xl px-4 pb-12 sm:px-6 lg:px-8">
+          <div className="mx-auto max-w-7xl px-4 pb-12 sm:px-6 lg:px-8 dark:text-slate-200">
             {children}
           </div>
         </main>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: 'class',
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
@@ -38,6 +39,18 @@ const config: Config = {
           '900': '#293e50',
           '950': '#172735',
         },
+        slate: {
+          '50': '#F8FAFC',
+          '100': '#F1F5F9',
+          '200': '#E2E8F0',
+          '300': '#CBD5E1',
+          '400': '#94A3B8',
+          '500': '#64748B',
+          '600': '#475569',
+          '700': '#334155',
+          '800': '#1E293B',
+          '900': '#0F172A',
+        }
       }
     },
   },
